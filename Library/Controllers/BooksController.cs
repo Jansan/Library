a8e6c68f3cfdb4d4c43031e630d3c698a5ad897f
Jansan <jansan-26@hotmail.com>
2019-08-08 12:11:52 +0200

    book model ,controller view created

---
+++ Library/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Random()
+        {
+            var book = new Book()
+            {
+                Name = "This is book"
+            };
+            return View(book);
+        }
+    }
+}
