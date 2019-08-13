06421dd6977979d38ff63f8f9155306c2ced9411
Jansan <jansan-26@hotmail.com>
2019-08-13 15:15:57 +0200

    added membershiptype

--- Library/Controllers/MembersController.cs
+++ Library/Controllers/MembersController.cs
@@ -4,20 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Library.Controllers
 {
     public class MembersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MembersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Members
         public ActionResult Index()
         {
-            var members = GetMembers();
+            var members = _context.Members;
             return View(members);
         }
         public ActionResult Details(int id)
         {
-            var member = GetMembers().SingleOrDefault(m => m.Id == id);
+            var member = _context.Members.Include(m => m.MembershipType).ToList();
 
             if (member == null)
                 return HttpNotFound();
