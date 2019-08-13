bad3d661f641d152dd2fca97c1992a41447a48e9
Jansan <jansan-26@hotmail.com>
2019-08-13 15:58:59 +0200

    add-migration Genres

--- Library/Migrations/201908131347078_Genres.cs
+++ Library/Migrations/201908131347078_Genres.cs
@@ -7,6 +7,15 @@ namespace Library.Migrations
     {
         public override void Up()
         {
+            Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Action and adventure')");
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (2, 'Alternate history')");
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (3, 'Anthology')");
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (4, 'Chick lit')");
+
+            Sql("INSERT INTO Genres (Id, Name) VALUES (5, 'Children s')");
         }
         
         public override void Down()
