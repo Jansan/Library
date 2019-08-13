a5eb42351a5f4f666382e87c6e27322afd934faa
Jansan <jansan-26@hotmail.com>
2019-08-13 16:08:42 +0200

    get book from data base

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -4,15 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Library.Controllers
 {
     public class BooksController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public BooksController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Books
         public ActionResult Index()
         {
-            var books = GetBooks();
+            var books = _context.Books.Include(b => b.Genre).ToList();
             return View(books);
         }
         public ActionResult Random()
@@ -26,13 +38,13 @@ namespace Library.Controllers
         {
             return Content("id=" + id);
         }
-        private IEnumerable<Book> GetBooks()
-        {
-            return new List<Book>
-            {
-               new Book {Id = 1, Name = "Karn"},
-               new Book {Id = 2, Name = "Lalla"}
-            };
-        }
+        //private IEnumerable<Book> GetBooks()
+        //{
+        //    return new List<Book>
+        //    {
+        //       new Book {Id = 1, Name = "Karn"},
+        //       new Book {Id = 2, Name = "Lalla"}
+        //    };
+        //}
     }
 }
